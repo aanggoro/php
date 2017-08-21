4008bddcf31a4895236553e4ef9f8c950c627e67
aanggoro <1511501288@student.budiluhur.ac.id>
2017-08-21 22:51:51 +0700

    add cnotroller error

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,5 +52,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['profil'] = 'home/halamanprofil';
 
-$route['404_override'] = '';
+$route['404_override'] = 'controllererror';
 $route['translate_uri_dashes'] = FALSE;
